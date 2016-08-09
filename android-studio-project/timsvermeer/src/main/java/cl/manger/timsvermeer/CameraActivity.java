69905d7035c2322df37a01bea6ccfd6daedcbdc6
germanger <givicencio@gmail.com>
2016-08-08 23:17:52 -0400

    camera2, colors and menu actions

--- android-studio-project/timsvermeer/src/main/java/cl/manger/timsvermeer/CameraActivity.java
+++ android-studio-project/timsvermeer/src/main/java/cl/manger/timsvermeer/CameraActivity.java
@@ -1,29 +1,68 @@
 package cl.manger.timsvermeer;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Point;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.util.Size;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class CameraActivity extends Activity {
 
     private static final int SELECT_PICTURE = 1;
     private Bitmap originalPhotoBitmap = null;
 
+    private TextureView textureView;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+    protected CameraCaptureSession cameraCaptureSessions;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    private String cameraId;
+    protected CameraDevice cameraDevice;
+    private Size previewSize;
+    private ImageReader imageReader;
+
     private Camera camera = null;
     private MySurfaceView mySurfaceView = null;
     private ImageView imageView = null;
+
+    private int rotation = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +87,11 @@ public class CameraActivity extends Activity {
         imageView = (ImageView) findViewById(R.id.imageView_photo);
         imageView.setImageBitmap(this.originalPhotoBitmap);
 
+        textureView = (TextureView) findViewById(R.id.texture);
+        textureView.setSurfaceTextureListener(textureListener);
+
         // Enable camera
-        enableCamera();
+        //enableCamera();
 
         SeekBar opacitySeekBar = (SeekBar) findViewById(R.id.seekBar_opacity);
         opacitySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -104,6 +146,139 @@ public class CameraActivity extends Activity {
         });
     }
 
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            // Transform you image captured size according to the surface width and height
+        }
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        }
+    };
+
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            cameraDevice = camera;
+            createCameraPreview();
+        }
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            cameraDevice.close();
+        }
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+    protected void createCameraPreview() {
+        try {
+            SurfaceTexture texture = textureView.getSurfaceTexture();
+            assert texture != null;
+            texture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+            Surface surface = new Surface(texture);
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(surface);
+            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback(){
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    if (cameraDevice == null) {
+                        return;
+                    }
+
+                    cameraCaptureSessions = cameraCaptureSession;
+                    updatePreview();
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = manager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            assert map != null;
+            previewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+                return;
+            }
+
+            manager.openCamera(cameraId, stateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("Camera Background");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+    protected void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void updatePreview() {
+        if(cameraDevice == null) {
+        }
+        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        try {
+            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void closeCamera() {
+        if (null != cameraDevice) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+        if (null != imageReader) {
+            imageReader.close();
+            imageReader = null;
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startBackgroundThread();
+        if (textureView.isAvailable()) {
+            openCamera();
+        } else {
+            textureView.setSurfaceTextureListener(textureListener);
+        }
+    }
+    @Override
+    protected void onPause() {
+        //closeCamera();
+        stopBackgroundThread();
+        super.onPause();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -119,20 +294,35 @@ public class CameraActivity extends Activity {
         int itemId = item.getItemId();
 
         if (itemId == R.id.action_focusCamera) {
-            camera.autoFocus(new Camera.AutoFocusCallback() {
-                @Override
-                public void onAutoFocus(boolean success, Camera camera) {
-                }
-            });
-
-            return true;
+            autoFocus();
+        } else if (itemId == R.id.action_rotatePhoto) {
+            rotatePhoto();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void autoFocus() {
+        camera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+            }
+        });
+    }
+
+    private void rotatePhoto() {
+        Matrix matrix = new Matrix();
+
+        rotation = rotation + 90;
+        matrix.postRotate(rotation);
+
+        Bitmap rotatedBitmap = Bitmap.createBitmap(originalPhotoBitmap , 0, 0, originalPhotoBitmap.getWidth(), originalPhotoBitmap.getHeight(), matrix, true);
+
+        imageView.setImageBitmap(rotatedBitmap);
+    }
+
     private void enableCamera() {
-        try {
+        /*try {
             camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
         } catch (Exception e) {
             Log.d("ERROR", "Failed to open camera: " + e.getMessage());
@@ -143,6 +333,6 @@ public class CameraActivity extends Activity {
 
             mySurfaceView = new MySurfaceView(this, camera);
             cameraFrameLayout.addView(mySurfaceView);
-        }
+        }*/
     }
 }
